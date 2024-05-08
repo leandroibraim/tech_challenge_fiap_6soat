ecbee28432fad5beaf5c037040716fec4af48c4a
igusvs <igoralves159@gmail.com>
2024-05-08 00:24:02 -0300

    remove arquivo nao utilizado

--- docker/setup-dynamo.sh
+++
@@ -1,6 +0,0 @@
-awslocal dynamodb create-table \
-    --table-name global01 \
-    --key-schema AttributeName=id,KeyType=HASH \
-    --attribute-definitions AttributeName=id,AttributeType=S \
-    --billing-mode PAY_PER_REQUEST \
-    --region ap-south-1
