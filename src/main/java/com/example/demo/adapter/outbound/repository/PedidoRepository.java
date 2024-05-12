d4df44ed8a62bba9c5fbcb00483a5cf3a39df404
igusvs <igoralves159@gmail.com>
2024-05-12 19:50:33 -0300

    remove imports

--- src/main/java/com/example/demo/adapter/outbound/repository/PedidoRepository.java
+++ src/main/java/com/example/demo/adapter/outbound/repository/PedidoRepository.java
@@ -2,8 +2,6 @@ package com.example.demo.adapter.outbound.repository;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.example.demo.adapter.outbound.repository.entity.PedidoEntity;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
