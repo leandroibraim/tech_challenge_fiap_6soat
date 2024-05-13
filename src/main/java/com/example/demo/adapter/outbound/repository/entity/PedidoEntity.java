79634d73a5117b492ac158f3fedbc907f763ffa3
igusvs <igoralves159@gmail.com>
2024-05-12 23:03:45 -0300

    Criar tabelas no docker compose e ajuste dynamoDbMapper

--- src/main/java/com/example/demo/adapter/outbound/repository/entity/PedidoEntity.java
+++ src/main/java/com/example/demo/adapter/outbound/repository/entity/PedidoEntity.java
@@ -4,23 +4,44 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
-import lombok.Builder;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Data
-@Builder
-@DynamoDBTable(tableName = "ProductInfo")
+@DynamoDBTable(tableName = "pedido")
 public class PedidoEntity {
 
-    @DynamoDBHashKey(attributeName = "numero_pedido")
+
     private String numeroPedido;
 
-    @DynamoDBRangeKey(attributeName = "status_pedido")
-    private String statusPedido;
 
-    @DynamoDBAttribute(attributeName = "data_pedido")
-    private LocalDateTime dataPedido;
+    private String dataPedido;
 
+    private String status;
+
+    @DynamoDBHashKey(attributeName = "numero_pedido")
+    public String getNumeroPedido() {
+        return numeroPedido;
+    }
+
+    public void setNumeroPedido(String numeroPedido) {
+        this.numeroPedido = numeroPedido;
+    }
+
+    @DynamoDBAttribute(attributeName = "status")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @DynamoDBRangeKey(attributeName = "data_pedido")
+    public String getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(String dataPedido) {
+        this.dataPedido = dataPedido;
+    }
 }
--- src/main/java/com/example/demo/config/DynamoDBClient.java
+++ src/main/java/com/example/demo/config/DynamoDBClient.java
@@ -28,5 +28,4 @@ public class DynamoDBClient {
 
     return new DynamoDBMapper(client, DynamoDBMapperConfig.DEFAULT);
     }
-
 }
