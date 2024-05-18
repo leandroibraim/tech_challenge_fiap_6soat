e1e32cc5e85bddea64093a9e87c8cfb2607d5a95
igusvs <igoralves159@gmail.com>
2024-05-18 20:50:35 -0300

    remover lombok classes domain

--- src/main/java/com/example/demo/core/domain/Composicao.java
+++ src/main/java/com/example/demo/core/domain/Composicao.java
@@ -1,10 +1,5 @@
 package com.example.demo.core.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Composicao {
 
     private Long idComposicao;
@@ -13,4 +8,43 @@ public class Composicao {
     private double precoUnitario;
     private Long numeroPedido;
 
+    public Long getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Long idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public Long getIdComposicao() {
+        return idComposicao;
+    }
+
+    public void setIdComposicao(Long idComposicao) {
+        this.idComposicao = idComposicao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Long getNumeroPedido() {
+        return numeroPedido;
+    }
+
+    public void setNumeroPedido(Long numeroPedido) {
+        this.numeroPedido = numeroPedido;
+    }
 }
--- src/main/java/com/example/demo/core/domain/Pedido.java
+++ src/main/java/com/example/demo/core/domain/Pedido.java
@@ -1,13 +1,7 @@
 package com.example.demo.core.domain;
 
-
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
-@Getter
-@Setter
 public class Pedido {
 
     private Long numeroPedido;
@@ -17,4 +11,51 @@ public class Pedido {
     private String status;
     private String etapa;
 
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Long getNumeroPedido() {
+        return numeroPedido;
+    }
+
+    public void setNumeroPedido(Long numeroPedido) {
+        this.numeroPedido = numeroPedido;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Composicao> getComposicao() {
+        return composicao;
+    }
+
+    public void setComposicao(List<Composicao> composicao) {
+        this.composicao = composicao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getEtapa() {
+        return etapa;
+    }
+
+    public void setEtapa(String etapa) {
+        this.etapa = etapa;
+    }
 }
